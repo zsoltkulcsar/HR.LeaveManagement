228ce6f2d34c7a73898826c73a65696b7dad1488
Zsolt Kulcsár <kulcsarzsolt15@gmail.com>
2025-01-16 14:32:15 +0200

    Created the domain project

---
+++ HR.LeaveManagement.Domain/Common/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace HR.LeaveManagement.Domain.Common
+{
+    public abstract class BaseEntity<T> where T : struct
+    {
+        public T Id { get; set; }
+
+        public DateTime? DateCreated { get; set; }
+
+        public DateTime? DateModified { get; set; }
+    }
+}
---
+++ HR.LeaveManagement.Domain/LeaveType.cs
@@ -0,0 +1,11 @@
+﻿using HR.LeaveManagement.Domain.Common;
+
+namespace HR.LeaveManagement.Domain
+{
+    public class LeaveType : BaseEntity<int>
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public int DefaultDays {  get; set; }
+    }
+}
