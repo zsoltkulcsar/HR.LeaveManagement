1e515c90f37ba6c588a34a17d628108647f31878
Zsolt Kulcsár <kulcsarzsolt15@gmail.com>
2025-01-16 14:42:25 +0200

    Added the data persistance layer

---
+++ HR.LeaveManagement.Application/Contracts/Persistence/ILeaveTypeRepository.cs
@@ -0,0 +1,11 @@
+﻿using HR.LeaveManagement.Domain;
+
+namespace HR.LeaveManagement.Application.Contracts.Persistence
+{
+    public interface ILeaveTypeRepository : IGenericRepository<LeaveType>
+    {
+
+    }
+
+
+}
