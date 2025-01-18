b3645a9fb232eeb4b0888975cc16227eae250aa8
Zsolt Kulcsár <kulcsarzsolt15@gmail.com>
2025-01-18 23:11:14 +0200

    Test

--- HR.LeaveManagement.Api/Controllers/LeaveRequestsController.cs
+++ HR.LeaveManagement.Api/Controllers/LeaveRequestsController.cs
@@ -1,6 +1,13 @@
-﻿namespace HR.LeaveManagement.Api.Controllers
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Api.Controllers
 {
     internal class LeaveRequestsController
     {
+        safa
     }
 }
